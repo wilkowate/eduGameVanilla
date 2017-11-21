4624fa303d7978d9f8add869291c5ab182426d43
ola <ola>
2017-11-21 16:25:54 +0100

    origin

--- src/routes.php
+++ src/routes.php
@@ -17,6 +17,26 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $this->renderer->render ( $response, 'footer.phtml', $args );
 });
 
+	$app->add(function ($req, $res, $next) {
+		$response = $next($req, $res);
+		return $response
+		->withHeader('Access-Control-Allow-Origin', '*')
+		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	});
+
+	$app->get('/world', function (Request $request, Response $response, array $args) {
+		// Sample log message
+		$this->logger->info("Slim-Skeleton '/' route");
+	
+		// Render index view
+		//return $this->renderer->render($response, 'index.phtml', $args);
+	
+		$this->renderer->render ( $response, 'header.phtml', $args );
+		$this->renderer->render ( $response, 'world.phtml', $args );
+		return $this->renderer->render ( $response, 'footer.phtml', $args );
+	});
+
 $app->get('/json', function (Request $request, Response $response, array $args) {
 	$response->withHeader('Content-Type', 'application/json');
 		
