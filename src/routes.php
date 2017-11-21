3ae63e710212682e71a2b83ed8f79f7c4ef380b8
ola <ola>
2017-11-21 16:12:30 +0100

    /json in use

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
@@ -15,4 +15,51 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     $this->renderer->render ( $response, 'header.phtml', $args );
     $this->renderer->render ( $response, 'town.phtml', $args );
     return $this->renderer->render ( $response, 'footer.phtml', $args );
+});
+
+$app->get('/json', function (Request $request, Response $response, array $args) {
+	$response->withHeader('Content-Type', 'application/json');
+		
+	$response->withHeader('Access-Control-Allow-Origin', '*');
+	//$response->withHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	//$response->withHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+		
+	$response->write(json_encode(
+		array(
+			"type"=>"FeatureCollection",
+			"crs"=> array(
+				"type"=>  "name",
+				"properties" => array(
+				"name"=> "EPSG:3857"
+				)
+			),
+			"features"=> array(
+		
+			array(
+				"type" => "Feature",
+				"properties"=> array("name" => "asdasd","id" => "412"),
+				"geometry" => array(
+					"type"=> "Polygon",    
+					"coordinates" => [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]]
+				)
+			)	, array(
+				"type" => "Feature",
+				"properties"=> array("name" => "asdasd","id" => "123"),
+				"geometry" => array("type"=> "LineString",    "coordinates" => [[4e6, -2e6], [8e6, 2e6]])
+				)
+			)
+		)
+	));
+});
+
+$app->get('/tree', function (Request $request, Response $response, array $args) {
+	return $this->renderer->render ( $response, 'tree.phtml', $args );
+});
+	
+$app->get('/n[{name}]', function (Request $request, Response $response, array $args) {
+	// Sample log message
+	$this->logger->info("Slim-Skeleton '/' route");
+	$this->renderer->render ( $response, 'header.phtml', $args );
+	$this->renderer->render ( $response, 'town.phtml', $args );
+	return $this->renderer->render ( $response, 'footer.phtml', $args );
 });
